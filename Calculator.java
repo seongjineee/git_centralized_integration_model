eb3417c7d5ab55a7940e6a7433d5760d210209e3
seongjineee <sungjin8506@naver.com>
2025-07-24 14:51:25 +0900

    performOperation 개발

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,21 @@
 public class Calculator{
-	public static double performOperation(
-	double num1, double num2, char operator) {
+	public static double performOperation(double num1, double num2, char operator) 
+	{double result = 0.0;
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            
+            default:
+                System.out.println("올바른 연산자를 입력하세요.");
+        }
+
+        return result;
+		
 }	public static void main(String[] args){
 }
 
